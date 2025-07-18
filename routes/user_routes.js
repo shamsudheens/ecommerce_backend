2e3cc116608eb6537d785c46cbbe83e2eaa5ff01
chechu776 <shameerchechu@gmail.com>
2025-07-18 16:38:58 +0530

    image upload complete

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,20 @@
 import express from "express"
 import { signup } from "../controller/user_controller.js"
+import multer from "multer"
+const router = express.Router()
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        const fileNameSplit = file.originalname.split(".");
+        const fileName = fileNameSplit[fileNameSplit?.length - 1]
+
+        req.filename = `${file.originalname}_${Date.now()}.${fileName}`
+        cb(null, req.filename)
+    }
+})
 
 const upload = multer({
     storage: storage
