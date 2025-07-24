021694dde7413d5bc2a2acf365ff47dd96ab32b7
chechu776 <shameerchechu@gmail.com>
2025-07-24 16:04:40 +0530

    shipping status updated

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,8 +1,9 @@
 import bcrypt from "bcrypt"
 import adminModel from "../models/admin.js"
 import userModel from "../models/user.js"
+import orderModel from "../models/order.js"
 
-export const login = async(req,res)=>{
+export const login = async (req, res) => {
     const { email, password } = req.body
     try {
         const user = await adminModel.findOne({ email })
@@ -14,42 +15,58 @@ export const login = async(req,res)=>{
             return res.json({ message: "incorrect password" })
         }
         req.session.adminid = user._id;
-        return res.json({ message: "welcome Admin..."})
+        return res.json({ message: "welcome Admin..." })
     }
     catch (err) {
         res.json({ message: err.message })
     }
 }
 
-export const logout = async(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err)
-        {
-            return res.status(404).json({message:"logout failed"})
+export const logout = async (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(404).json({ message: "logout failed" })
         }
-        else
-        {
-            return res.status(404).json({message:"logout successfull"})
+        else {
+            return res.status(404).json({ message: "logout successfull" })
         }
     })
 }
 
-export const userstatus =async(req,res)=>{
-    const id=req.params.id;
-    try{
-        const data=await userModel.findByIdAndUpdate(id,{
-            status:req.body.status
+export const userstatus = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const data = await userModel.findByIdAndUpdate(id, {
+            status: req.body.status
         })
-        if(!data)
-        {
-            return res.status(404).json({message:"user not found"})
+        if (!data) {
+            return res.status(404).json({ message: "user not found" })
         }
-        else
-        {
-            return res.status(200).json({message:"status updated successfully"})
+        else {
+            return res.status(200).json({ message: "status updated successfully" })
         }
     }
-    catch(err){
-        return res.status(400).json({message:"Internal server error"})
+    catch (err) {
+        return res.status(400).json({ message: "Internal server error" })
+    }
+}
+
+export const shippingstatus = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const data = await orderModel.findByIdAndUpdate(id, {
+            shippingStatus: req.body.shippingStatus
+        })
+        if (!data) {
+            return res.status(404).json({ message: "order not found" })
+        }
+        else {
+            return res.status(200).json({ message: "shipping status updated successfully" })
+        }
+    }
+    catch (err) {
+        console.log(err);
+        
+        return res.status(400).json({ message: "Internal server error" })
     }
 }
--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,10 +1,18 @@
 import express from "express"
-import {login,logout,userstatus} from "../controller/admin_controller.js"
+import {login,logout,userstatus,shippingstatus} from "../controller/admin_controller.js"
 
 const router=express.Router()
 
 router.get("/login",login)
 router.get("/logout",logout)
+
+router.get("/",(req,res,next)=>{
+    if(req.session.adminid)next()
+    else
+        return res.status(403).json({message:"Access only for admins"})
+})
+
 router.patch("/userstatus/:id",userstatus)
+router.patch("/shippingstatus/:id",shippingstatus)
 
 export default router
