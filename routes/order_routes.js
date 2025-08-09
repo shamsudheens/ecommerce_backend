1ceeb5cb67c8cd069f226d4e56a66eee29ab5141
chechu776 <shameerchechu@gmail.com>
2025-08-09 16:14:37 +0530

    middleware setup done

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -1,14 +1,15 @@
 import express from "express"
 import { createOrder, paymentStatus, showAllOrders, showOrder, cancelOrder } from "../controller/order_controller.js"
 import { userMiddleware } from "../middleware/user_middleware.js"
+import { adminMiddleware } from "../middleware/admin_middleware.js"
 const router = express.Router()
 
+router.get("/showAllOrders",adminMiddleware, showAllOrders)
+router.patch("/paymentStatus/:id",adminMiddleware, paymentStatus)
 
 router.use(userMiddleware)
 
 router.post("/createOrder", createOrder)
-router.patch("/paymentStatus/:id", paymentStatus)
-router.get("/showAllOrders", showAllOrders)
 router.get("/showOrder/:id", showOrder)
 router.delete("/cancelOrder/:id", cancelOrder)
 
