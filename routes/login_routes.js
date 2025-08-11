5c5061c64699238aeee12d7ddbe65ed122983df0
chechu776 <shameerchechu@gmail.com>
2025-08-11 12:42:13 +0530

    user login done

--- routes/login_routes.js
+++ routes/login_routes.js
@@ -2,6 +2,6 @@ import express from "express"
 import {login} from "../controller/login_controller.js"
 const router=express.Router()
 
-router.get("/",login)
+router.post("/",login)
 
 export default router
